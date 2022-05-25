41ab5b43596eef008a42c551e5655affa62e94ef
JohnOK <john.ok.esp@gmail.com>
2022-05-25 23:47:16 +0200

    added factorial function (recursive and linear versions)

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,21 @@
 def factorial(num):
-	# your code here
+	# recursive version
+	# if num == 0:
+	# 	return 1
+	# elif num == 1:
+	# 	return 1
+	# else:
+	# 	return num * factorial(num - 1)
+
+	# linear version
+	answer = num
+	i = num
+
+	if num == 0:
+		return 1
+	else:
+		while i > 1:
+			answer *= i - 1
+			i -= 1
+
+	return answer
